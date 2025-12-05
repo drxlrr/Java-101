414cc3c1758a7582807890b81d6b4a750a30c34b
Drexler B. Anijas <dbanijas@headoffice.corp.bpi.com.ph>
2025-12-05 15:48:28 +0800

    add m2_act2

---
+++ Act2/main.java
@@ -0,0 +1,15 @@
+package m2.act2;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		whiteboard wb = new whiteboard();
+		
+		int total = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + total);
+	}
+
+}
