fc69a20ef3731335c54b36c89332f7f204cb4b80
Drexler B. Anijas <dbanijas@headoffice.corp.bpi.com.ph>
2025-12-09 17:38:26 +0800

    add act3

---
+++ M2_ACT3/Car.java
@@ -0,0 +1,23 @@
+package m2.act3;
+
+public class Car {
+	
+	String brand="";
+	String model="";
+	
+	void CarInfo() {
+		
+		System.out.println("Brand Name: " + brand);
+		System.out.println("Model Name: " + model);
+		
+	}
+	
+	
+	void CarInfo(String brand,String model) {
+		
+	System.out.println("Brand Name: " + brand);
+	System.out.println("Model Name: " + model);
+	
+		
+	}
+}
---
+++ M2_ACT3/Main.java
@@ -0,0 +1,19 @@
+package m2.act3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Car car = new Car();
+		car.model = "E-bike 3 wheels";
+		car.brand = "Lucky Lion";
+		car.CarInfo();
+		System.out.println("==========================");
+		car.CarInfo("N-WOW", "E-Bike 4 wheels");
+		
+		
+	}
+
+}
