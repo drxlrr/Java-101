4535b0219c3c313eb92f9f83b637152b4a9fde80
Drexler B. Anijas <dbanijas@headoffice.corp.bpi.com.ph>
2025-12-09 17:44:36 +0800

    act5

---
+++ act5/MSWord.java
@@ -0,0 +1,22 @@
+package m2.Execise4;
+
+public class MSWord extends Program {
+	
+	public MSWord(String name) {
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		String message = "Opening MS Word...";
+		this.setIsRunning(true);
+		System.out.println(message);
+	}
+	
+	@Override
+	public void stop() {
+		String message = "Stopping MS Word...";
+		this.setIsRunning(false);
+      System.out.println(message);
+	}
+}
