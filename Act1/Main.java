149c3b4707d0eb9b5459eb020c72e4ad308435ed
Drexler B. Anijas <dbanijas@headoffice.corp.bpi.com.ph>
2026-01-09 08:26:57 +0800

    add m4_act1

---
+++ Act1/Main.java
@@ -0,0 +1,45 @@
+package m4.act1;
+
+
+public class Main {
+
+    static class BankAccount {
+
+        public String getAccountName(String accountNumber) {
+            if ("ACC-001".equals(accountNumber)) {
+                return "Juan Dela Cruz";
+            } else if ("ACC-002".equals(accountNumber)) {
+                return "Maria Santos";
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public static void testCase(String accountNumber) {
+        System.out.println();
+        System.out.println("Looking up account: " + accountNumber);
+
+        BankAccount bank = new BankAccount();
+        try {
+            String name = bank.getAccountName(accountNumber);
+
+            String upperName = name.toUpperCase();
+
+            System.out.println("Account holder: " + upperName);
+        } catch (NullPointerException ex) {
+            System.out.println("Error: Account not found!");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=== Bank Account Name Display ===");
+
+        testCase("ACC-001");
+
+        testCase("ACC-999");
+
+        System.out.println();
+        System.out.println("=== Program completed successfully! ===");
+    }
+}
